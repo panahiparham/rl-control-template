6d53c7eddcc426b5610a3da8dea4bddccad144b4
Parham Panahi <panahiparham@gmail.com>
2026-06-15 19:19:26 -0600

    feat: update vmap to be vmap compatible

--- scripts/local.py
+++ scripts/local.py
@@ -2,11 +2,10 @@ import sys
 import os
 sys.path.append(os.getcwd() + '/src')
 
-import random
+import io
 import argparse
 import subprocess
-from functools import partial
-from multiprocessing.pool import Pool
+import contextlib
 from glob import glob
 
 from utils.results import gather_missing_indices
@@ -18,45 +17,41 @@ parser.add_argument('-e', type=str, nargs='+', required=True)
 parser.add_argument('--entry', type=str, default='src/main.py')
 parser.add_argument('--results', type=str, default='./')
 
-def count(pre, it):
-    print(pre, 0, end='\r')
-    for i, x in enumerate(it):
-        print(pre, i + 1, end='\r')
-        yield x
-
-    print()
-
 if __name__ == "__main__":
     cmdline = parser.parse_args()
 
-    # Expand glob patterns in the -e arguments
+    # Expand glob patterns
     expanded_paths = []
     for pattern in cmdline.e:
         matches = glob(pattern, recursive=True)
         if matches:
             expanded_paths.extend(matches)
         else:
-            expanded_paths.append(pattern)  # Keep original if no matches
-
+            expanded_paths.append(pattern)
     cmdline.e = sorted(expanded_paths)
 
-    pool = Pool()
+    # Suppress the built-in print from gather_missing_indices; we print our own format.
+    with contextlib.redirect_stdout(io.StringIO()):
+        e_to_missing = gather_missing_indices(cmdline.e, cmdline.runs, loader=Experiment.load, base=cmdline.results)
 
-    cmds = []
-    e_to_missing = gather_missing_indices(cmdline.e, cmdline.runs, loader=Experiment.load)
+    # Print per-file status: "path missing/total"
+    grand_total = 0
     for path in cmdline.e:
         exp = Experiment.load(path)
+        total = exp.numPermutations() * cmdline.runs
+        grand_total += total
+        missing = len(e_to_missing[path])
+        print(f'{path} {missing}/{total}')
 
-        indices = count(path, e_to_missing[path])
-        for idx in indices:
-            exe = f'python {cmdline.entry} --silent -e {path} -i {idx}'
-            cmds.append(exe)
+    total_missing = sum(len(v) for v in e_to_missing.values())
+    remaining = total_missing
 
-    print(len(cmds))
-    random.shuffle(cmds)
-    res = pool.imap_unordered(partial(subprocess.run, shell=True, stdout=subprocess.PIPE), cmds, chunksize=1)
-    for i, _ in enumerate(res):
-        sys.stderr.write(f'\r{i+1}/{len(cmds)}')
-    sys.stderr.write('\n')
+    for path, missing in e_to_missing.items():
+        if not missing:
+            continue
+        idx_args = ' '.join(map(str, missing))
+        subprocess.run(f'python {cmdline.entry} --silent -e {path} -i {idx_args}', shell=True, check=True)
+        remaining -= len(missing)
+        print(f'{path} done  [{grand_total - remaining}/{grand_total} completed]')
 
-    pool.close()
+    print(f'all done!  [{grand_total}/{grand_total} completed]')
